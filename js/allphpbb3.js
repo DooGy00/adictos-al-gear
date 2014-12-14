1730c56ce9d5044203e488295978a4c7f594072b
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-14 03:09:51 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -782,7 +782,7 @@ if (tm) {
         },
     };
     var zeditoronbeforeunload = $("#editor-post-button").find("span");
-    var advancebutton = ;
+   
     window.onbeforeunload = function (e) {
         if (zeditor.textarea.value != "" && !$(".edit-mode").length) {
             return _userdata.username + " tienes texto en el editor que podrias perder";
