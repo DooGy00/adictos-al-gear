812b38b4a591a76ab4d413d86b9aeca51d933463
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-14 03:55:09 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -463,7 +463,7 @@ if (tm) {
                 zeditor.textarea.placeholder = _userdata.username + " escribe un comentario...";
                 if (zeditor.textarea.value != "") {
                     if (confirm(_userdata.username + " si deseas publicar tu mensaje presiona ENVIAR,  de continuar  perderas lo escrito")) {
-                        location.href = zeditor.url;
+                       
                     } else {
                         $("#editor-send-button ").css("background", "gold");
                     }
