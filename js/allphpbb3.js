b0c67e635fb5590debc791406bffcbfbc01470d6
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-15 18:34:23 +0000

    cambie de lugar prefix y de lugar el status

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -1,3 +1,29 @@
+function lang_vi(b) {
+    b = b.toLowerCase();
+    b = b.replace(/\u00e0|\u00e1|\u1ea1|\u1ea3|\u00e3|\u00e2|\u1ea7|\u1ea5|\u1ead|\u1ea9|\u1eab|\u0103|\u1eb1|\u1eaf|\u1eb7|\u1eb3|\u1eb5/g, "a");
+    b = b.replace(/\u00e8|\u00e9|\u1eb9|\u1ebb|\u1ebd|\u00ea|\u1ec1|\u1ebf|\u1ec7|\u1ec3|\u1ec5/g, "e");
+    b = b.replace(/\u00ec|\u00ed|\u1ecb|\u1ec9|\u0129/g, "i");
+    b = b.replace(/\u00f2|\u00f3|\u1ecd|\u1ecf|\u00f5|\u00f4|\u1ed3|\u1ed1|\u1ed9|\u1ed5|\u1ed7|\u01a1|\u1edd|\u1edb|\u1ee3|\u1edf|\u1ee1/g, "o");
+    b = b.replace(/\u00f9|\u00fa|\u1ee5|\u1ee7|\u0169|\u01b0|\u1eeb|\u1ee9|\u1ef1|\u1eed|\u1eef/g, "u");
+    b = b.replace(/\u1ef3|\u00fd|\u1ef5|\u1ef7|\u1ef9/g, "y");
+    b = b.replace(/\u0111/g, "d");
+    b = b.replace(/\W+/g, "-");
+    return b.replace(/^\-+|\-+$/g, "");
+}
+var titulosprefixA = $(".lastpost").find("a"),
+    titulosprefixB = $(".news_topic_title"),
+    titulosprefixC = $(".topic-title").find("a"),
+    titulosprefixD = $(".page-title").find("a");
+$(function () {
+    titulosprefixA.add(titulosprefixB).add(titulosprefixC).add(titulosprefixD).html(function () {
+        var b = this.innerHTML;
+        if (/^\[(tema|staff| sondeo |nota|codigo|actualizado|ayuda|tutorial|guia|consulta|punbb|phpbb3|nuevo|noticia|tv|presentacion|afiliacion|publicidad|recursos|anuncio|duda|evento|sugerencia|Premiun|resuelto|soporte|pedido|jquery|javascript|html|css|novedades)\]/i.test(b)) {
+            return b.replace(/\[(.+)\]/, function (c) {
+                return '<span class="prefix ' + lang_vi(c) + '">' + c.replace(/[\[\]]/g, "") + "</span>";
+            });
+        }
+    });
+});
 if ($("#fa_menulist").length) {
     var status_box = {
         lang: {
@@ -37,7 +63,7 @@ if ($("#fa_menulist").length) {
         }
     };
     if (_userdata.session_logged_in == "1") {
-        $("#fa_menulist").prepend('<div id="AAGstatus"></div>');
+        $("#fa_usermenu").append('<div id="AAGstatus"></div>');
         status_box.init("profile_field_13_1");
     }
 }
@@ -1322,32 +1348,7 @@ $("#fa_notifications").on("click", function () {
     $(this).attr("style", "background-image: url(http://adictosalgear.org/images/bell.png)!important;background-position: right!important;background-repeat: no-repeat!important;");
 });
 
-function lang_vi(b) {
-    b = b.toLowerCase();
-    b = b.replace(/\u00e0|\u00e1|\u1ea1|\u1ea3|\u00e3|\u00e2|\u1ea7|\u1ea5|\u1ead|\u1ea9|\u1eab|\u0103|\u1eb1|\u1eaf|\u1eb7|\u1eb3|\u1eb5/g, "a");
-    b = b.replace(/\u00e8|\u00e9|\u1eb9|\u1ebb|\u1ebd|\u00ea|\u1ec1|\u1ebf|\u1ec7|\u1ec3|\u1ec5/g, "e");
-    b = b.replace(/\u00ec|\u00ed|\u1ecb|\u1ec9|\u0129/g, "i");
-    b = b.replace(/\u00f2|\u00f3|\u1ecd|\u1ecf|\u00f5|\u00f4|\u1ed3|\u1ed1|\u1ed9|\u1ed5|\u1ed7|\u01a1|\u1edd|\u1edb|\u1ee3|\u1edf|\u1ee1/g, "o");
-    b = b.replace(/\u00f9|\u00fa|\u1ee5|\u1ee7|\u0169|\u01b0|\u1eeb|\u1ee9|\u1ef1|\u1eed|\u1eef/g, "u");
-    b = b.replace(/\u1ef3|\u00fd|\u1ef5|\u1ef7|\u1ef9/g, "y");
-    b = b.replace(/\u0111/g, "d");
-    b = b.replace(/\W+/g, "-");
-    return b.replace(/^\-+|\-+$/g, "");
-}
-var titulosprefixA = $(".lastpost").find("a"),
-    titulosprefixB = $(".news_topic_title"),
-    titulosprefixC = $(".topic-title").find("a"),
-    titulosprefixD = $(".page-title").find("a");
-$(function () {
-    titulosprefixA.add(titulosprefixB).add(titulosprefixC).add(titulosprefixD).html(function () {
-        var b = this.innerHTML;
-        if (/^\[(tema|staff| sondeo |nota|codigo|actualizado|ayuda|tutorial|guia|consulta|punbb|phpbb3|nuevo|noticia|tv|presentacion|afiliacion|publicidad|recursos|anuncio|duda|evento|sugerencia|Premiun|resuelto|soporte|pedido|jquery|javascript|html|css|novedades)\]/i.test(b)) {
-            return b.replace(/\[(.+)\]/, function (c) {
-                return '<span class="prefix ' + lang_vi(c) + '">' + c.replace(/[\[\]]/g, "") + "</span>";
-            });
-        }
-    });
-});
+
 (function (b, a, c) {
     (function (d) {
         if (typeof define === "function" && define.amd) {
