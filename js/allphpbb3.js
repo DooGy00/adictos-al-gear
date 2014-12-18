8f779257f32e8d9002234c9e1bbb8fe05a90ec30
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-18 02:05:06 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -764,35 +764,38 @@ if (tm) {
             b = document.getElementById("editor-loading");
             d == "on" ? (b.style.display = "") : (b.style.display = "none")
         },
-        advance: function() {
+       advance: function () {
     if ($(".edit-mode").length || $(".quote-mode").length) {
         location.href = zeditor.url;
         window.onbeforeunload = false
     }
-    var editA =!$(".edit-mode").length;
-    var quotA =!$(".quote-mode").length;
-
-    if ( editA && zeditor.textarea.value != "") {
-        window.onbeforeunload = false;
-        if (confirm(_userdata.username + " de continuar  perderas lo escrito ¿Deseas ir al editor avanzado?")) {
+    var editA = !$(".edit-mode").length;
+    var quotA = !$(".quote-mode").length;
+    if (editA) {
+        if (editA && zeditor.textarea.value != "") {
+            window.onbeforeunload = false;
+            if (confirm(_userdata.username + " de continuar  perderas lo escrito ¿Deseas ir al editor avanzado?")) {
+                location.href = zeditor.url
+            } else {
+                zeditor.textarea.focus()
+            }
+        }
+        if (editA && zeditor.textarea.value === "") {
             location.href = zeditor.url
-        } else {
-            zeditor.textarea.focus()
         }
     }
-    if (editA && zeditor.textarea.value === "") {
-        location.href = zeditor.url
-    }
-    if ( quotA && zeditor.textarea.value != "") {
-        window.onbeforeunload = false;
-        if (confirm(_userdata.username + " de continuar  perderas lo escrito ¿Deseas ir al editor avanzado?")) {
-            location.href = zeditor.url
-        } else {
-            zeditor.textarea.focus()
+    if (quotA) {
+        if (quotA && zeditor.textarea.value != "") {
+            window.onbeforeunload = false;
+            if (confirm(_userdata.username + " de continuar  perderas lo escrito ¿Deseas ir al editor avanzado?")) {
+                location.href = zeditor.url
+            } else {
+                zeditor.textarea.focus()
+            }
         }
-    }
-    if (quotA && zeditor.textarea.value === "") {
-        location.href = zeditor.url
+        if (quotA && zeditor.textarea.value === "") {
+            location.href = zeditor.url
+        }
     }
 },
         avatar: function(g, d) {
