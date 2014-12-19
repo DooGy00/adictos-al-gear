8f0142162ffddc04f1dff5d44297d66775481f48
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-19 18:33:03 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -924,9 +924,11 @@ if (tm) {
     });
     if ($(".baivietdai").length) {
         $(".pbutton1").add(".pbutton2").add(".quote a").add(".edit a").add(".mp").add(".postnumber a").on("click", function() {
+        $(".baivietdai").attr("style","overflow:auto!important");
         $('body,html').stop().animate({
             scrollTop: $('#ze-editor').offset().top
         }, 100);
+        $("#ze-editor").offset({top:100});
         zeditor.textarea.focus();
         return false
     });
