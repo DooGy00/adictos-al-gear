9d1d587078de7e8aecf3e9a86e99e06c9104fcc1
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2014-12-19 06:18:19 +0000

    ok

--- trunk/js/allphpbb3.js
+++ trunk/js/allphpbb3.js
@@ -916,7 +916,7 @@ a.hide().siblings().show()
     var split = $('a[href*="mode=split"]').attr("href");
     var merge = $('a[href*="merge"]').attr("href");
     if (level === 1 | level === 2 | trash) {
-        $(".left-box").first().after('<div id="moderation_tool" class="act_mod"><p class="mod" title="Moderar tema">Moderar</p></div>');
+        $(".tema-info").after('<div id="moderation_tool" class="act_mod"><p class="mod" title="Moderar tema">Moderar</p></div>');
         $("#moderation_tool").append('<div id="popw" class="action_mod"><div class="mod_hover"></div><div class="popwinner"><li><a href="' + del + '">Eliminar este tema</a></li><li><a href="' + trash + '">Enviar a la papelera</a></li><li><a href="' + move + '">Mover este tema</a></li><li><a href="' + lockunlock + '">Bloquear/Desbloquear</a></li><li><a href="' + split + '">Separar este tema</a></li><li><a href="' + merge + '">Fusionar el tema</a></li></div></div>');
         $(".mod").on("click", function() {
             $(".action_mod").slideToggle(300)
