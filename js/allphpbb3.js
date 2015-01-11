cafa833a39c7ef4dea01b5c324f4ac9f44c4d6e4
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-01-11 08:43:07 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -102,7 +102,7 @@ if ($("#fa_menulist").length) {
 		var estadotexto = $("#AAGstatus_input").val();
 		$.post("/privmsg", {
 			subject: 'Mensaje automático',
-			message: _userdata.username + ' actualizo su estado: [color=#FF0000][b]"' + estadotexto +'"[/b][/color]',
+			message: _userdata.username + ' actualizo su estado: [color=#FF0000][b][i]"' + estadotexto +'"[/i][/b][/color]',
 			username: 'Historial',
 			mode: "post_profile",
 			folder: "profile",
