55ba17bb783ed62bffb7b1ea159c58dbbef7b260
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-01-15 07:49:59 +0000

    ok

--- trunk/js/allphpbb3.js
+++ trunk/js/allphpbb3.js
@@ -513,7 +513,7 @@ if (tm) {
             $(".baivietdai").height("auto");
             $("body,html").stop().animate({
                 scrollTop: $("#ze-editor-form").offset().top
-            });
+            },0);
             zeditor.textarea.focus()
         })
     }
