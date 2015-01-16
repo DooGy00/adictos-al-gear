3e008eaa36089b01334ab00032afe2f38a7e4c83
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-01-16 23:33:31 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -517,10 +517,8 @@ $('.tinypic').on("click", function () {
     });
     if ($(".baivietdai").length) {
         $(".pbutton1").add(".pbutton2").add(".quote a").add(".edit a").add(".mp").add(".postnumber a").on("click", function () {
-            $(".baivietdai").height("auto");
-            $("body,html").stop().animate({
-                scrollTop: $("#ze-editor-form").offset().top
-            },100);
+            $(".baivietdai").height("100%");
+            
             zeditor.textarea.focus()
         })
     }
