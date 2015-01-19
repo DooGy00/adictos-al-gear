ada08dee50fa737cd2282c169c6bf2f981c78990
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-01-19 00:07:43 +0000

    ok

--- trunk/js/allphpbb3.js
+++ trunk/js/allphpbb3.js
@@ -722,6 +722,7 @@ setTimeout(function () {
             B + '><input class="button2" type="submit" value="No" name="cancel" id="quickLogoutClose" ' + B + "></fieldset></form></div>";
         if (!document.getElementById("logout")) {
             $('a[href*="/login"]').on("click", function () {
+              $('#quickLoginPanel').find('form')[0].redirect.value = window.location.href;
                 if (!document.getElementById("quickLoginPanel")) {
                     $("body").append(u);
                     $("#quickLoginPanel").css("left", t.offsetX + "%").css("top", "-25%").animate({
@@ -771,10 +772,7 @@ setTimeout(function () {
             })
         }
     });
-       if (!document.getElementById("quickLogoutPanel")) {
-      $('a[href*="/login"]').on("click", function () {
-           document.getElementById('quickLoginPanel').getElementsByTagName('form')[0].redirect.value = window.location.href;
-      });}
+      
 }, 500);
 setTimeout(function () {
     if (tm) {
