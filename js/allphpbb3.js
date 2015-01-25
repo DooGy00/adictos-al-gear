b9412c15898137104f5e772c33f40f5d2389db2b
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-01-25 19:04:37 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -283,7 +283,7 @@ $('.tinypic').on("click", function () {
 $("#editor-textarea").on("keyup",function () {
     if ("@" === $(this).val().split("")[$(this).val().length - 1]) {
         var b = prompt("Introduce el nombre del usuario a etiquetar").replace(/\s/g, "+");
-        var c = $(this)
+        var c = $(this).val();
         $(this).val(($(this).val() ? $(this).val() + "" : "") + b);
         (b);
        
