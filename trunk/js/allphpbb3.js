54645c997e5c7755b4a440cf8fb622f9b2c1205a
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-02-06 23:22:54 +0000

    ok

--- trunk/js/allphpbb3.js
+++ trunk/js/allphpbb3.js
@@ -182,7 +182,10 @@ if ($("#fa_menulist").length) {
                     return s.innerHTML = this.lang.too_short = this.lang.too_short
                 }
                 var t = document.getElementById("logout");
-                t && (t = t.href, t = t.substring(t.indexOf("tid=") + 4, t.indexOf("&key")), t = "id=" + this.id.substring(this.id.lastIndexOf("_") + 1) + '&active=1&content=[["' + this.id + '", "' + this.input.value + '"]]&tid=' + t + "&user=" + this.user_id, $.post("/ajax_profile.forum?jsoncallback=jQuery1", t, function (a) {  $.post("/privmsg", {
+                t && (t = t.href, t = t.substring(t.indexOf("tid=") + 4, t.indexOf("&key")), t = "id=" + this.id.substring(this.id.lastIndexOf("_") + 1) + '&active=1&content=[["' + this.id + '", "' + this.input.value + '"]]&tid=' + t + "&user=" + this.user_id, $.post("/ajax_profile.forum?jsoncallback=jQuery1", t, function (a) { 
+                    0 < a.indexOf(status_box.input.value) ? (status_box.input.value = "", s.innerHTML = status_box.lang.updated, setTimeout("document.getElementById('AAGstatus_notice').innerHTML=\" \"", 2500)) : s.innerHTML = status_box.lang.error
+                      }, function(notif) {
+                       $.post("/privmsg", {
             subject: 'Mensaje automático',
             message: _userdata.username + ' actualizo su estado: [color=#FF0000][b][i]"' + estadotexto + '"[/i][/b][/color]',
             username: 'Historial',
@@ -190,8 +193,10 @@ if ($("#fa_menulist").length) {
             folder: "profile",
             post: "Send"
         });
-                    0 < a.indexOf(status_box.input.value) ? (status_box.input.value = "", s.innerHTML = status_box.lang.updated, setTimeout("document.getElementById('AAGstatus_notice').innerHTML=\" \"", 2500)) : s.innerHTML = status_box.lang.error
-                }))
+                      
+                      }
+                
+                ))
             }
         }
     };
