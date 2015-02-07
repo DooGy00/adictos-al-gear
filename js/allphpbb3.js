0f37a82748992a9e6860610288a1884460a1fc2b
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-02-07 21:03:47 +0000

    ok

--- js/allphpbb3.js
+++ js/allphpbb3.js
@@ -566,6 +566,7 @@ if (sub) {
 }
 if(mp){$(".post-icon").find("img").replaceWith("<buttom>Enviar un MP</buttom>");
 $(".profile-icons.mps").find('a[href*="/privmsg?mode=quote&"]').html('<span class="mpquote" style="font-size:10px">Citar</span>');
+$('.post-icon').find("a").text("Responder MP a " + $(".postprofile").find("strong").first().text());
 }
 var mpindex = $(".forabg").length;
 if (mp && mpindex) {
