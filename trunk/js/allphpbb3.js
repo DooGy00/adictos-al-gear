46d28ff53ffaba59996f95535bf998fb2e7e5e3d
drgvd75@gmail.com <drgvd75@gmail.com@c083b895-4d8c-9580-381d-66ffab74b512>
2015-02-08 03:12:01 +0000

    ok

--- trunk/js/allphpbb3.js
+++ trunk/js/allphpbb3.js
@@ -570,9 +570,6 @@ if (sub) {
     })
 }
 if(mp){
-if($("form").find("p").text() === "¿Estás seguro de que deseas borrar estos mensajes?"){$(".panel").attr("style","min-height:300px");
-$("#content").addClass("formMp");
-}
 (function(O) {
 O(function() {
    /privmsg\?mode=post/.test(window.location) && O(document.forms.post.post).on("click", function() {
